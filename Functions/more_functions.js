a0416c8e013a867012226d0e3c7f1188144c68bd
Destiny Moncrief <D.moncrief4@gmail.com>
2024-04-30 15:16:47 -0400

    completed new exercises

---
+++ Functions/more_functions.js
@@ -0,0 +1,29 @@
+"use strict"
+
+function displayMailingLabel(name,address,city,state,zip) {
+    console.log(name);
+    console.log(addres);
+    console.log(city);
+    console.log(state);
+    console.log(zip);
+}
+
+
+function addNumbers(num1,num2) {
+    let result = Number(num1) + Number(num2);
+    console.log(num1 + " + " + num2 + " = " + result)
+}
+
+
+function displayReceipt(totalDue,amountPaid) {
+    let changeDue = totalDue - amountPaid;
+    if(changeDue > 0) {
+        let message = " Please pay the due amount of " + changeDue
+        console.log( "TotalDue: $" + totalDue);
+        console.log( "Amount Paid: $" + amountPaid);
+
+        console.log("Change Due: " + message);
+
+
+    }
+}
---
+++ Functions/simple_functions.js
@@ -0,0 +1,22 @@
+"use strict"
+
+
+function FavoriteThings() {
+    console.log("Destiny");
+    console.log("Nightmare on Elm Street.");
+    console.log("Kendrick Lamar");
+}
+
+
+function whyImHere() {
+    console.log("i want to learn to code. ");
+}
+
+
+function favoritePlace() {
+    console.log("Atlanta, i love the city. ");
+}
+
+FavoriteThings();
+whyImHere();
+favoritePlace();
