847f7829156e6a1cad0a7a1afa1b30e861c9218d
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-02 14:34:36 -0400

    fixed exercise 2

--- Dates/DateScripts/more_date_conversions.js
+++ Dates/DateScripts/more_date_conversions.js
@@ -1,12 +1,12 @@
 "use strict"
 
 // My Birthday
-let myBirthday = new Date("1997-02-07");
+let myBirthday = new Date("1997-02-08");
 console.log("My Birthday:", myBirthday.toLocaleString());
 
 
 // My mom's birthday
-let momsBirthday = new Date("1968-09-30");
+let momsBirthday = new Date("1968-09-31");
 console.log("Moms Birthday", momsBirthday.toLocaleString());
 
 
