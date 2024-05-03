78463634796ab97e208d99dded257fad462bbf72
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-03 11:21:54 -0400

    Created demos

---
+++ Scripts/index2.js
@@ -0,0 +1,29 @@
+
+
+
+window.onload = init;
+
+
+// buttons
+function init(){
+const hb = document.getElementById("helloBtn");
+const gb = document.getElementById("goodbyeBtn");
+
+
+}
+
+
+// output
+function onHelloBtnClick(){
+const o = document.getElementById("output");
+o.innerHTMl = "HELLO!"
+
+
+}
+
+
+function onGoodbyeBtnClick(){
+const output = document.getElementById("output");
+o.innerHTMl = "GOODBYE!"
+
+}
---
+++ Scripts/index4.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+
+
+window.onload = init;
+
+
+
+function init(){
+const helloBtn = document.getElementById("helloBtn");
+const goodbyeBtn = document.getElementById("goodbyeBtn");
+
+
+}
+
+
+
+function onHelloBtnClick(){
+const output = document.getElementById("output");
+output.innerHTMl = "HELLO!"
+
+
+}
+
+
+function onGoodbyeBtnClick(){
+const output = document.getElementById("output");
+output.innerHTMl = "GOODBYE!"
+
+}
