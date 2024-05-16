abca627dcece5bd6013f51c631e790c1f54f3c2e
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-16 13:02:46 -0400

    made changes

--- Functions/more_functions.js
+++ Functions/more_functions.js
@@ -1,29 +1,26 @@
-"use strict"
+"use strict";
 
-function displayMailingLabel(name,address,city,state,zip) {
+function displayMailingLabel(name, address, city, state, zip) {
     console.log(name);
-    console.log(addres);
+    console.log(address); 
     console.log(city);
     console.log(state);
     console.log(zip);
 }
 
-
-function addNumbers(num1,num2) {
+function addNumbers(num1, num2) {
     let result = Number(num1) + Number(num2);
-    console.log(num1 + " + " + num2 + " = " + result)
+    console.log(num1 + " + " + num2 + " = " + result);
 }
 
-
-function displayReceipt(totalDue,amountPaid) {
+function displayReceipt(totalDue, amountPaid) {
+    totalDue = Number(totalDue); 
+    amountPaid = Number(amountPaid); 
     let changeDue = totalDue - amountPaid;
-    if(changeDue > 0) {
-        let message = " Please pay the due amount of " + changeDue
-        console.log( "TotalDue: $" + totalDue);
-        console.log( "Amount Paid: $" + amountPaid);
-
+    if (changeDue > 0) {
+        let message = "Please pay the due amount of " + changeDue;
+        console.log("TotalDue: $" + totalDue);
+        console.log("Amount Paid: $" + amountPaid);
         console.log("Change Due: " + message);
-
-
     }
 }
